b555ddd5581805e6848d5455b94ee3db867ddd7b
Tercio Jose <terciob19@hotmail.com>
2025-06-09 17:49:12 -0300

    Translation in CurseForge

--- locales/koKR.lua
+++ locales/koKR.lua
@@ -1,4 +1,5 @@
 do
+    --translate here: https://legacy.curseforge.com/wow/addons/details-damage-meter-mythic/localization
     local addonId = ...
     local languageTable = DetailsFramework.Language.RegisterLanguage(addonId, "koKR")
     local L = languageTable
